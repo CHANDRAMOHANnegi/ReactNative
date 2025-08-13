ebe3d74c68bc17d849f23cab7e865fa8a1cccd34
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2025-08-13 19:05:21 +0530

    chore(app): initial commit

--- App.tsx
+++ App.tsx
@@ -4,13 +4,15 @@
  *
  * @format
  */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './packages/app/home';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,19 +21,15 @@ function App() {
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
+      <Home />
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text>Hello</Text>
     </View>
   );
 }
